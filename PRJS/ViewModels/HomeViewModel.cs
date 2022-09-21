e2dc0a01efb7b0f366ff22b99943b024f4408322
devazamili <38879571+devatrivanus18@users.noreply.github.com>
2022-09-21 10:00:15 +0700

    add calculate

--- PRJS/ViewModels/HomeViewModel.cs
+++ PRJS/ViewModels/HomeViewModel.cs
@@ -26,6 +26,9 @@ public class HomeViewModel : BaseViewModel
     private ObservableCollection<Product> _products = new ObservableCollection<Product>();
     public ObservableCollection<Product> Products { get => _products; set => SetProperty(ref _products, value); }
 
+    private float _total;
+    public float Total { get => _total; set => SetProperty(ref _total, value); }
+
     public Product product { get; set; }
 
     public ICommand SaveCommand { get; set; }
@@ -33,6 +36,7 @@ public class HomeViewModel : BaseViewModel
     public ICommand SaveDetailCommand { get; set; }
     public ICommand OnSelectedItemCommand { get; set; }
     public ICommand OnEditingItemCommand { get; set; }
+    public ICommand CalculateCommand { get; set; }
     public DatabaseService DatabaseService { get; set; }
     public HomeViewModel()
     {
@@ -41,12 +45,27 @@ public class HomeViewModel : BaseViewModel
         SaveCommand = new Command(SaveInvoice);
         MoveToGridPageCommand = new Command(MoveToGridPage);
         OnSelectedItemCommand = new Command(AddItemToInvoiceSellUnit);
+        OnEditingItemCommand = new Command(OnEditingCell);
+        CalculateCommand = new Command(CalculateTotal);
         Products.Add(new Product { unitNo = "1", eName = "Apple", aName = "يالللقرت٤", itemNo = 1, price = 100000 });
         Products.Add(new Product { unitNo = "2", eName = "Grape", aName = "يالللقرت٤", itemNo = 2, price = 1300000 });
         Products.Add(new Product { unitNo = "3", eName = "Banana", aName = "يالللقرت٤", itemNo = 3, price = 1600000 });
     }
 
+    private void CalculateTotal(object obj)
+    {
+        Total = ListInvoiceSellUnit.Sum(x => x.total);
+    }
 
+    private void OnEditingCell(object obj)
+    {
+        
+        var item = obj as InvoiceSellUnit;
+
+        item.total = item.quantity * item.price - item.discount;
+        item.taxRate1_Total = item.taxRate1_Percentage / 100 * item.total;
+        item.totalPlusTax = item.total + item.taxRate1_Total;
+    }
     private void SaveInvoice(object obj)
     {
         saveInvoiceSell();
@@ -63,7 +82,7 @@ public class HomeViewModel : BaseViewModel
             aName = item.aName,
             price = item.price,
             unitNo = item.unitNo,
-            itemNo = item.itemNo
+            itemNo = item.itemNo,
         });
     }
 
@@ -73,10 +92,7 @@ public class HomeViewModel : BaseViewModel
     public async void saveInvoiceSell()
     {
         InvoiceSell invoicesell = InvoiceSell;
-
-
-        InvoiceSellUnit invoicesellunit = InvoiceSellUnit;
-
+        invoicesell.subNetTotal = Total;
         await DatabaseService.SaveInvoiceSellAsync(invoicesell);
     }
 
